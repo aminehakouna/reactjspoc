c32c4211f6939a41cbfc4c3313cc811635449179
Amine Hakouna <amine.hakouna@gmail.com>
2019-02-22 20:19:30 +0100

    sample component

---
+++ src/components/homePage.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let React = require('react');
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>Home Page</div>
+        );
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+
+
 ReactDOM.render(<p>Hello Amine!</p>, document.getElementById('root'));
